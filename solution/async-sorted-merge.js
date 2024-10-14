8a0f7111f15c13a02a1c8ff4c08d8cf956e4efa5
O'Marie J. Pemberton <omarie-pemberton@OMaries-MacBook-Pro.local>
2024-10-14 14:05:07 -0400

    solution

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,9 +1,55 @@
 "use strict";
 
+const AsyncMinHeapLogSourceSorter = require("../lib/async-min-heap-log-source-sorter");
+
 // Print all entries, across all of the *async* sources, in chronological order.
 
 module.exports = (logSources, printer) => {
-  return new Promise((resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
+    const asyncMinHeapLogSourceSorter = new AsyncMinHeapLogSourceSorter();
+
+    /*
+    Since the log sources are already sorted, we want to asynchronously:
+    1) Go through each log source and grab the first logEntry
+    2) Update the logEntry with the index of the logSource where this log originated from. This will allow us
+      to quickly retrieve the log again for the next logEntry
+    3) Add the initial logEntries to the min-heap for sorting
+    4) Process remaining entries until the heap is empty
+    */
+    for (let i = 0; i < logSources.length; i++) {
+      const logEntry = await logSources[i].popAsync();
+      logEntry.logSourceIndex = i;
+      await asyncMinHeapLogSourceSorter.insert(logEntry);
+    }
+
+    // Printing logic
+    while (!asyncMinHeapLogSourceSorter.isEmpty()) {
+      let logEntry = await asyncMinHeapLogSourceSorter.getMinLogEntry();
+      printer.print(logEntry);
+
+      /*
+      Get next log entry from that log's source. It's possible that this is the next earliest entry.
+      Once added to the heap, this entry will be sorted with the existing entries.
+      */
+      const currentLogSource = logSources[logEntry.logSourceIndex];
+
+      try {
+        const nextLogEntry = await currentLogSource.popAsync();
+        if (nextLogEntry) {
+          nextLogEntry.logSourceIndex = logEntry.logSourceIndex;
+          await asyncMinHeapLogSourceSorter.insert(nextLogEntry);
+          // asyncMinHeapLogSourceSorter.printHeapInOrder();
+        }
+      } catch (error) {
+        console.error(
+          `Error occurred during async processing for entry ${nextLogEntry}:`,
+          error
+        );
+        reject(error);
+      }
+    }
+
+    printer.done();
     resolve(console.log("Async sort complete."));
   });
 };
--- solution/sync-sorted-merge.js
+++ solution/sync-sorted-merge.js
@@ -1,7 +1,43 @@
 "use strict";
 
+const MinHeapLogSourceSorter = require("../lib/min-heap-log-source-sorter");
+
 // Print all entries, across all of the sources, in chronological order.
 
 module.exports = (logSources, printer) => {
+  const minHeapLogSourceSorter = new MinHeapLogSourceSorter();
+  /*
+  Since the log sources are already sorted, we want to:
+  1) Go through each log source and grab the first logEntry
+  2) Update the logEntry with the index of the logSource where this log originated from. This will allow us
+     to quickly retrieve the log again for the next logEntry
+  3) Add the initial logEntries to the min-heap for sorting
+  4) Process remaining entries until the heap is empty
+  */
+  logSources.map((logSource, index) => {
+    const logEntry = logSource.pop();
+    logEntry.logSourceIndex = index;
+    minHeapLogSourceSorter.insert(logEntry);
+  });
+
+  // printing logic
+  while (!minHeapLogSourceSorter.isEmpty()) {
+    let logEntry = minHeapLogSourceSorter.getMinLogEntry(); // get earliest date log entry
+    printer.print(logEntry);
+
+    /*
+    Get next log entry from that log's source. It's possible that this is the next earliest entry.
+    Once added to the heap, this entry will be sorted with the existing entries.
+    */
+    let currentLogSource = logSources[logEntry.logSourceIndex];
+    let nextLogEntry = currentLogSource.pop();
+
+    if (nextLogEntry) {
+      nextLogEntry.logSourceIndex = logEntry.logSourceIndex; // update the index of where this log entry derived from
+      minHeapLogSourceSorter.insert(nextLogEntry);
+    }
+  }
+  printer.done();
+
   return console.log("Sync sort complete.");
 };
