589ce704794c5ae33101b4504516d0c6becf7f69
O'Marie J. Pemberton <omarie-pemberton@OMaries-MacBook-Pro.local>
2024-10-17 11:28:06 -0400

    updated async solution

--- solution/async-sorted-merge.js
+++ solution/async-sorted-merge.js
@@ -1,52 +1,48 @@
 "use strict";
 
-const AsyncMinHeapLogSourceSorter = require("../lib/async-min-heap-log-source-sorter");
+const MinHeapLogSourceSorter = require("../lib/min-heap-log-source-sorter");
 
 // Print all entries, across all of the *async* sources, in chronological order.
 
 module.exports = (logSources, printer) => {
   return new Promise(async (resolve, reject) => {
-    const asyncMinHeapLogSourceSorter = new AsyncMinHeapLogSourceSorter();
+    const minHeapLogSourceSorter = new MinHeapLogSourceSorter();
+    const exhaustedLogSources = new Set();
 
     /*
-    Since the log sources are already sorted, we want to asynchronously:
-    1) Go through each log source and grab the first logEntry
-    2) Update the logEntry with the index of the logSource where this log originated from. This will allow us
-      to quickly retrieve the log again for the next logEntry
-    3) Add the initial logEntries to the min-heap for sorting
-    4) Process remaining entries until the heap is empty
+    The intent now is to pre-populate the heap until all log sources are exhausted. This can be done efficiently
+    with Promise.all() and calling popAsync(). Essentially we are 'batching' the entries into the heap.
     */
-    for (let i = 0; i < logSources.length; i++) {
-      const logEntry = await logSources[i].popAsync();
-      logEntry.logSourceIndex = i;
-      await asyncMinHeapLogSourceSorter.insert(logEntry);
+    while (
+      !logSources.every((logSource) => exhaustedLogSources.has(logSource))
+    ) {
+      // asynchronously get the earliest logEntry from every logSource
+      const nextEntries = await Promise.all(
+        logSources.map(async (logSource) => {
+          if (!exhaustedLogSources.has(logSource)) {
+            const logEntry = await logSource.popAsync();
+            if (logEntry) {
+              return logEntry;
+            } else {
+              exhaustedLogSources.add(logSource);
+            }
+          }
+          return null;
+        })
+      );
+
+      // add entries to the min-heap
+      for (const logEntry of nextEntries) {
+        if (logEntry) {
+          minHeapLogSourceSorter.insert(logEntry);
+        }
+      }
     }
 
-    // Printing logic
-    while (!asyncMinHeapLogSourceSorter.isEmpty()) {
-      let logEntry = await asyncMinHeapLogSourceSorter.getMinLogEntry();
+    // all entries are added and sorted, print
+    while (!minHeapLogSourceSorter.isEmpty()) {
+      let logEntry = minHeapLogSourceSorter.getMinLogEntry();
       printer.print(logEntry);
-
-      /*
-      Get next log entry from that log's source. It's possible that this is the next earliest entry.
-      Once added to the heap, this entry will be sorted with the existing entries.
-      */
-      const currentLogSource = logSources[logEntry.logSourceIndex];
-
-      try {
-        const nextLogEntry = await currentLogSource.popAsync();
-        if (nextLogEntry) {
-          nextLogEntry.logSourceIndex = logEntry.logSourceIndex;
-          await asyncMinHeapLogSourceSorter.insert(nextLogEntry);
-          // asyncMinHeapLogSourceSorter.printHeapInOrder();
-        }
-      } catch (error) {
-        console.error(
-          `Error occurred during async processing for entry ${nextLogEntry}:`,
-          error
-        );
-        reject(error);
-      }
     }
 
     printer.done();
